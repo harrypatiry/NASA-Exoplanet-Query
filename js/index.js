8a7fc90290a986a9ae6485e9590521c871c935d1
harrypatiry <harrypatiry@gmail.com>
2022-05-01 15:28:26 -0400

    display

--- js/index.js
+++ js/index.js
@@ -3,13 +3,31 @@ async function cumulative(url) {
     const response = await fetch(url);
     let data = await response.json();
     console.log(data);
+    //removeEmpty(data)
     show(data);
 }
-
 cumulative(api_url);
 
+// function removeEmpty(obj) {
+//     Object.keys(obj).forEach(function(key) {
+//       (obj[key] && typeof obj[key] === 'object') && removeEmpty(obj[key]) ||
+//       (obj[key] === '' || obj[key] === null || obj[key] === undefined) && delete obj[key]
+//     });
+//     show(obj)
+//   };
+
 function show(data) {
-    //let div = document.createElement("p");
-    //div.append(data.kepid);
-    console.log(data[0].kepid)
+    for (let i = 0; data.length; i++) {
+        let name = document.createElement("div");
+        name.innerHTML = 'Name: ' + data[i].kepler_name;
+        let temp = document.createElement("div");
+        temp.innerHTML = 'Temperature: ' + data[i].koi_teq;
+        let a = document.getElementById('cumulative')
+        let b = document.getElementById('cumulative')
+        a.appendChild(name);
+        b.appendChild(temp);
+    }
+    //let c = document.getElementById("cumulative");
+    //c.append(data[0]);
+    //console.log(data[0].kepid)
 }
