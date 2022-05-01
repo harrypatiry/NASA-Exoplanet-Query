9ea76235687ddd276c017e9d8cdd0d7ae751b52d
harrypatiry <harrypatiry@gmail.com>
2022-05-01 14:03:02 -0400

    json

--- js/index.js
+++ js/index.js
@@ -1,8 +1,15 @@
-const api_url = 'https://exoplanetarchive.ipac.caltech.edu/cgi-bin/nstedAPI/nph-nstedAPI?table=cumulative';
+const api_url = 'https://exoplanetarchive.ipac.caltech.edu/cgi-bin/nstedAPI/nph-nstedAPI?table=cumulative&format=json';
 async function cumulative(url) {
     const response = await fetch(url);
-    let data = await response.text();
-    console.log(data)
+    let data = await response.json();
+    console.log(data);
+    show(data);
 }
 
 cumulative(api_url);
+
+function show(data) {
+    //let div = document.createElement("p");
+    //div.append(data.kepid);
+    console.log(data[0].kepid)
+}
