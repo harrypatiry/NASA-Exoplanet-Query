eb2521f3bf35a0c8318a86579a5d001d7ba7dd9a
harrypatiry <harrypatiry@gmail.com>
2022-05-02 15:08:34 -0400

    adding more data to output + styles

--- js/index.js
+++ js/index.js
@@ -6,8 +6,8 @@ let dataArray = [];
 //search for current input values
 search.addEventListener('keyup', (e) => {
     const searched = e.target.value.toLowerCase();
-    const filtered = dataArray.filter((planet) => {
-        return planet.kepler_name.toLowerCase().includes(searched);
+    const filtered = dataArray.filter((o) => {
+        return o.kepler_name.toLowerCase().includes(searched);
     })
     //console.log(filtered)
     results(filtered);
@@ -35,7 +35,13 @@ function results(filtered) {
         return `
         <li class='planet'>
             <h2>${data.kepler_name}</h2>
-            <p>Temperature: ${data.koi_teq}</p>
+            <div class='data'>
+                <p>Orbital Period: ${data.koi_period}</p>
+                <p>Temperature(kelvin): ${data.koi_steff}</p>
+                <p>Surface Gravity: ${data.koi_slogg}</p>
+                <p>Metallicity: ${data.koi_smet}</p>
+                <p>Mass: ${data.koi_smass}</p>
+            </div>
         </li>
         `;
     }).join('');
