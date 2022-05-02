d90a7b3bae154f64a4feba0abb30eadda5088eae
harrypatiry <harrypatiry@gmail.com>
2022-05-02 12:57:42 -0400

    search and destroy

--- js/index.js
+++ js/index.js
@@ -1,17 +1,37 @@
 const api_url = 'https://exoplanetarchive.ipac.caltech.edu/cgi-bin/nstedAPI/nph-nstedAPI?table=cumulative&format=json';
 const planets = document.getElementById('planets')
 const search = document.getElementById('search');
-let data = [];
+let dataArray = [];
+
+//search for current input values
+search.addEventListener('keyup', (e) => {
+    const searched = e.target.value.toLowerCase();
+    const filtered = dataArray.filter((planet) => {
+        return planet.kepler_name.toLowerCase().includes(searched);
+    })
+    //console.log(filtered)
+    results(filtered);
+});
+
+//fetch api 
 async function cumulative(url) {
     const response = await fetch(url);
-    let data = await response.json();
-    console.log(data);
-    show(data);
+    data = await response.json();
+    //filter out null kepler name values
+    dataArray = data.filter(data => {
+        let isValid = true
+        if (!data.kepler_name) 
+            isValid = false;
+        return isValid
+    })
+    console.log(dataArray);
 }
 cumulative(api_url);
 
-function show(data) {
-    const htmlString = data.map((data) => {
+//show filtered search results
+function results(filtered) {
+    const htmlData = filtered.map((data) => {
+
         return `
         <li class='planet'>
             <h2>${data.kepler_name}</h2>
@@ -19,5 +39,5 @@ function show(data) {
         </li>
         `;
     }).join('');
-    planets.innerHTML = htmlString;
+    planets.innerHTML = htmlData;
 }
