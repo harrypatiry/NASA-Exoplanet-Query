0772243d1746806bdd5d4c81be67ca3ad9171a21
harrypatiry <harrypatiry@gmail.com>
2022-05-01 21:55:50 -0400

    map instead

--- js/index.js
+++ js/index.js
@@ -1,33 +1,23 @@
 const api_url = 'https://exoplanetarchive.ipac.caltech.edu/cgi-bin/nstedAPI/nph-nstedAPI?table=cumulative&format=json';
+const planets = document.getElementById('planets')
+const search = document.getElementById('search');
+let data = [];
 async function cumulative(url) {
     const response = await fetch(url);
     let data = await response.json();
     console.log(data);
-    //removeEmpty(data)
     show(data);
 }
 cumulative(api_url);
 
-// function removeEmpty(obj) {
-//     Object.keys(obj).forEach(function(key) {
-//       (obj[key] && typeof obj[key] === 'object') && removeEmpty(obj[key]) ||
-//       (obj[key] === '' || obj[key] === null || obj[key] === undefined) && delete obj[key]
-//     });
-//     show(obj)
-//   };
-
 function show(data) {
-    for (let i = 0; data.length; i++) {
-        let name = document.createElement("div");
-        name.innerHTML = 'Name: ' + data[i].kepler_name;
-        let temp = document.createElement("div");
-        temp.innerHTML = 'Temperature: ' + data[i].koi_teq;
-        let a = document.getElementById('cumulative')
-        let b = document.getElementById('cumulative')
-        a.appendChild(name);
-        b.appendChild(temp);
-    }
-    //let c = document.getElementById("cumulative");
-    //c.append(data[0]);
-    //console.log(data[0].kepid)
+    const htmlString = data.map((data) => {
+        return `
+        <li class='planet'>
+            <h2>${data.kepler_name}</h2>
+            <p>Temperature: ${data.koi_teq}</p>
+        </li>
+        `;
+    }).join('');
+    planets.innerHTML = htmlString;
 }
