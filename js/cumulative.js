57b5a4b81c107eb949530e11027d3092cef7369a
harrypatiry <harrypatiry@gmail.com>
2022-05-02 21:53:17 -0400

    new api page

---
+++ js/cumulative.js
@@ -0,0 +1,48 @@
+const api_url = 'https://exoplanetarchive.ipac.caltech.edu/cgi-bin/nstedAPI/nph-nstedAPI?table=cumulative&format=json';
+const planets = document.getElementById('planets')
+const search = document.getElementById('search');
+let dataArray = [];
+
+//search for current input values
+search.addEventListener('keyup', (e) => {
+    const searched = e.target.value.toLowerCase();
+    const filtered = dataArray.filter((o) => {
+        return o.kepler_name.toLowerCase().includes(searched);
+    })
+    //console.log(filtered)
+    results(filtered);
+});
+
+//fetch api 
+async function cumulative(url) {
+    const response = await fetch(url);
+    data = await response.json();
+    //filter out null kepler name values
+    dataArray = data.filter(data => {
+        let isValid = true
+        if (!data.kepler_name) 
+            isValid = false;
+        return isValid
+    })
+    console.log(dataArray);
+}
+cumulative(api_url);
+
+//show filtered search results
+function results(filtered) {
+    const htmlData = filtered.map((data) => {
+        return `
+        <li class='planet'>
+            <h2>${data.kepler_name}</h2>
+            <div class='data'>
+                <p>Temperature(kelvin): ${data.koi_steff}</p>
+                <p>Surface Gravity: ${data.koi_slogg}</p>
+                <p>Metallicity: ${data.koi_smet}</p>
+                <p>Mass: ${data.koi_smass}</p>
+                <p>Age: ${data.koi_sage}</p>
+            </div>
+        </li>
+        `;
+    }).join('');
+    planets.innerHTML = htmlData;
+}
--- js/mission.js
+++ js/mission.js
@@ -1,4 +1,4 @@
-const api_url = 'https://exoplanetarchive.ipac.caltech.edu/cgi-bin/nstedAPI/nph-nstedAPI?table=cumulative&format=json';
+const api_url = 'https://exoplanetarchive.ipac.caltech.edu/cgi-bin/nstedAPI/nph-nstedAPI?table=missionstars&format=json';
 const planets = document.getElementById('planets')
 const search = document.getElementById('search');
 let dataArray = [];
@@ -7,7 +7,7 @@ let dataArray = [];
 search.addEventListener('keyup', (e) => {
     const searched = e.target.value.toLowerCase();
     const filtered = dataArray.filter((o) => {
-        return o.kepler_name.toLowerCase().includes(searched);
+        return o.star_name.toLowerCase().includes(searched);
     })
     //console.log(filtered)
     results(filtered);
@@ -20,7 +20,7 @@ async function cumulative(url) {
     //filter out null kepler name values
     dataArray = data.filter(data => {
         let isValid = true
-        if (!data.kepler_name) 
+        if (!data.star_name) 
             isValid = false;
         return isValid
     })
@@ -31,16 +31,11 @@ cumulative(api_url);
 //show filtered search results
 function results(filtered) {
     const htmlData = filtered.map((data) => {
-
         return `
         <li class='planet'>
-            <h2>${data.kepler_name}</h2>
+            <h2>${data.star_name}</h2>
             <div class='data'>
-                <p>Orbital Period: ${data.koi_period}</p>
-                <p>Temperature(kelvin): ${data.koi_steff}</p>
-                <p>Surface Gravity: ${data.koi_slogg}</p>
-                <p>Metallicity: ${data.koi_smet}</p>
-                <p>Mass: ${data.koi_smass}</p>
+                <p>Temperature(kelvin):</p>
             </div>
         </li>
         `;
